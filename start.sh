e1aeee683a482bf87e118aec1b12af5742e251d1
xėṟø <99372941+xero-nova@users.noreply.github.com>
2024-10-05 10:55:16 +1000

    feat: update start.sh to use variables and capture logs

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+world_id="83cad395edb8d0f1912fec89508d8a1d"
+server_port=15151
+
 while true; do
-    ./blockheads_server171 -o WORLD_ID -p PORT
+    ./blockheads_server171 -o $world_id -p $server_port >> ~/GNUstep/Library/ApplicationSupport/TheBlockheads/saves/$world_id/console.log 2>&1
     sleep 1
 done
